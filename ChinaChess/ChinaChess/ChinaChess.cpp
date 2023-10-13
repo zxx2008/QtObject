71b44e737619421d2226436f6b680afc0628964f
zxx2008 <2665954635@qq.com>
2023-10-13 09:33:00 +0800

    象棋鼠标事件

--- ChinaChess/ChinaChess/ChinaChess.cpp
+++ ChinaChess/ChinaChess/ChinaChess.cpp
@@ -1,5 +1,6 @@
 #include "ChinaChess.h"
 #include <QDesktopWidget>
+#include <QMouseEvent>
 
 ChinaChess::ChinaChess(QWidget* parent)
     : QMainWindow(parent), m_ptrMainView(new QGraphicsView), m_ptrMainScene(new QGraphicsScene)
@@ -46,3 +47,49 @@ ChinaChess::ChinaChess(QWidget* parent)
 }
 
 ChinaChess::~ChinaChess() {}
+
+//通过坐标使相应棋子置为选择状态
+void ChinaChess::selectChess(int x, int y) {
+    clearSelectChess();     //清除所有的棋子选中状态
+    for (auto item : m_lstChess) {
+        //找到对应棋子判断是否存活，如果存活才能被选中
+        if (x == item->getX() && y == item->getY()) {
+            //需要判断其选择的棋子颜色是否和出子的方颜色相同
+            if (m_Role == Red && item->getChessType() <= Bing_R) {
+                item->setSelectedStatus(true);
+            }
+            else if (m_Role == Black && item->getChessType > Bing_R) {
+                item->setSelectedStatus(true);
+            }
+        }
+    }
+}
+
+void ChinaChess::clearSelectChess() {
+    for (auto item : m_lstChess) {
+        item->setSelectedStatus(false);
+    }
+}
+
+void ChinaChess::mousePressEvent(QMouseEvent* event) {
+    int x = -1, y = -1;
+    //根据鼠标选中的位置获取棋盘坐标
+    QPoint Coordinates = getChessboardCoordinates(event->x(), event->y());
+    x = Coordinates.x();
+    y = Coordinates.y();
+
+    //已选中棋子的情况
+    if (this->getSelectedChess() != QPoint(-1, -1)) {
+        if (m_Role == Red) {
+            for (auto& item : m_lstChess) {
+                if (x != -1 && y != -1 && item->getX() == x && item->getY() == y && item->getChessType() <= Bing_R) {
+                    selectChess(x, y);
+                    return;
+                }
+            }
+        }
+        else if (m_Role == Black) {
+            
+        }
+    }
+}
--- ChinaChess/ChinaChess/x64/Debug/moc/moc_chess_man.cpp
+++ ChinaChess/ChinaChess/x64/Debug/moc/moc_chess_man.cpp
@@ -1,19 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'chess_man.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include <memory>
 #include "../../../chess_man.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'chess_man.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.14.2. It"
+#error "This file was generated using the moc from 5.9.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -44,7 +43,7 @@ QT_MOC_LITERAL(2, 18, 0) // ""
 static const uint qt_meta_data_ChessMan[] = {
 
  // content:
-       8,       // revision
+       7,       // revision
        0,       // classname
        0,    0, // classinfo
        1,   14, // methods
@@ -66,7 +65,7 @@ static const uint qt_meta_data_ChessMan[] = {
 void ChessMan::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        auto *_t = static_cast<ChessMan *>(_o);
+        ChessMan *_t = static_cast<ChessMan *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->sigClick(); break;
@@ -75,7 +74,7 @@ void ChessMan::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
         {
-            using _t = void (ChessMan::*)();
+            typedef void (ChessMan::*_t)();
             if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&ChessMan::sigClick)) {
                 *result = 0;
                 return;
@@ -85,14 +84,10 @@ void ChessMan::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject ChessMan::staticMetaObject = { {
-    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
-    qt_meta_stringdata_ChessMan.data,
-    qt_meta_data_ChessMan,
-    qt_static_metacall,
-    nullptr,
-    nullptr
-} };
+const QMetaObject ChessMan::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_ChessMan.data,
+      qt_meta_data_ChessMan,  qt_static_metacall, nullptr, nullptr}
+};
 
 
 const QMetaObject *ChessMan::metaObject() const
